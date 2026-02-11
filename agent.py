09f14febe35511efa8ccff892e2aa7cf636e127f
frayce.yoann <frayceyoann@gmail.com>
2026-02-11 19:13:52 +0100

    feat(core): implement first version of rational agent with budgeting, premium access, auto-payment and retry logic

--- agent.py
+++ agent.py
@@ -1,5 +1,6 @@
 PREMIUM_DATA_COST = 0.05  # € simulé
 ESTIMATION_ERROR_FACTOR = 0.15  # 15% erreur estimation
+import requests
 
 def decide_action(state, budget):
 
@@ -13,3 +14,22 @@ def decide_action(state, budget):
         return "buy_premium_data"
     else:
         return "use_basic_data"
+
+def get_premium_data():
+
+    # tentative sans paiement
+    response = requests.get("http://127.0.0.1:8000/premium-weather")
+
+    if response.status_code == 402:
+
+        # payer
+        payment = requests.post("http://127.0.0.1:8000/pay")
+        payment_id = payment.json()["payment_id"]
+
+        # rejouer requête avec preuve paiement
+        response = requests.get(
+            "http://127.0.0.1:8000/premium-weather",
+            params={"payment_id": payment_id}
+        )
+
+    return response.json()
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from environment import get_environment_state
-from agent import decide_action
+from agent import decide_action, get_premium_data
+from fastapi import HTTPException
+from uuid import uuid4
+
 
 app = FastAPI()
 
@@ -15,9 +18,43 @@ def run_agent():
 
     if decision == "buy_premium_data":
         agent_budget -= 0.05
+        premium_data = get_premium_data()
+    else:
+        premium_data = None
+
 
     return {
-        "environment": state,
-        "decision": decision,
-        "remaining_budget": agent_budget
+    "environment": state,
+    "decision": decision,
+    "premium_data": premium_data,
+    "remaining_budget": agent_budget
+    }
+
+
+# stockage simple des paiements validés
+valid_payments = set()
+
+@app.get("/premium-weather")
+def premium_weather(payment_id: str = None):
+
+    if payment_id not in valid_payments:
+        raise HTTPException(
+            status_code=402,
+            detail="Payment Required"
+        )
+
+    # données premium plus précises
+    return {
+        "solar_production_precise": 85.0  # valeur fixe pour démo
+    }
+
+@app.post("/pay")
+def make_payment():
+
+    payment_id = str(uuid4())
+    valid_payments.add(payment_id)
+
+    return {
+        "status": "payment_successful",
+        "payment_id": payment_id
     }
